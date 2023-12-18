f857330cefb22347bb9bc3e865948fd90b1b12ef
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-18 02:22:32 +0100

    simplifies example

--- examples/sine.rs
+++ examples/sine.rs
@@ -11,12 +11,20 @@ fn main() -> Result<()> {
     let config = Config::default();
     let device = Device::new("test".to_string());
 
+    let channels = config.channels as usize;
     let phase_inc = (440.0 * TWO_PI) / config.sample_rate as f32;
-    let mut phase = 0.0;
+    let mut phase: f32 = 0.0;
 
     // callbacks
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-    let data_fn = move |data: &mut [f32]| write_data(data, config.channels, &mut phase, &phase_inc);
+    let data_fn = move |data: &mut [f32]| {
+        for frame in data.chunks_mut(channels) {
+            for sample in frame {
+                *sample = (phase).sin();
+            }
+            phase += phase_inc;
+        }
+    };
 
     let stream = device.build_output_stream(&config, data_fn, err_fn)?;
     stream.play()?;
@@ -27,13 +35,4 @@ fn main() -> Result<()> {
 
     drop(stream);
     Ok(())
-}
-
-fn write_data(data: &mut [f32], channels: u8, phase: &mut f32, phase_inc: &f32) {
-    for frame in data.chunks_mut(channels as usize) {
-        for sample in frame {
-            *sample = (*phase).sin();
-        }
-        *phase += phase_inc;
-    }
 }
